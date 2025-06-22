b101fa4a6327e045991e95ac2faf68b46fe63e82
callmeATHUL <athulpsudheer05@gmail.com>
2025-06-22 23:55:15 +0300

    yellow to red

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoyellowuringBuilds: true,
+    ignoreduringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
